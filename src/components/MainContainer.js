dec1dd0941b08c591752ba727e3b6169aabf3ca2
Robert Gaspar <gaspar.robert7@gmail.com>
2021-12-01 09:21:19 -0600

    commit tree 79881baf1c814ab781d11ca2c2f137a9e21b2018

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
